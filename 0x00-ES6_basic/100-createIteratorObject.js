22a640f518aaf32d2fb6b8941947b591802a1c95
MrtnOmwenga <martin36449@gmail.com>
2023-06-26 22:36:14 +0000

    Eslint

--- 0x00-ES6_basic/100-createIteratorObject.js
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -1,9 +1,9 @@
 export default function createIteratorObject(report) {
   const result = [];
-  for (const department in report.allEmployees) {
-    report.allEmployees[department].map((employee) => {
-	    result.push(employee);
-    });
+  for (const department of Object.keys(report.allEmployees)) {
+    for (const employee of report.allEmployees[department]) {
+      result.push(employee);
+    }
   }
   return result;
 }
