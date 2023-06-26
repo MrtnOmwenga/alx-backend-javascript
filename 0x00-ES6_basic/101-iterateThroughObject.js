3de7b7dbf2ea1ac186dc60de1e19bc469188dc44
MrtnOmwenga <martin36449@gmail.com>
2023-06-26 22:48:39 +0000

    Changing result from list to string

--- 0x00-ES6_basic/101-iterateThroughObject.js
+++ 0x00-ES6_basic/101-iterateThroughObject.js
@@ -1,7 +1,7 @@
 export default function iterateThroughObject(reportWithIterator) {
-  let result = [];
+  let result = '';
   for (const employee of reportWithIterator) {
-    result = result.length === 0 ? [result + employee] : [`${result} | ${employee}`];
+    result = result === '' ? result + employee : `${result} | ${employee}`;
   }
   return result;
 }
