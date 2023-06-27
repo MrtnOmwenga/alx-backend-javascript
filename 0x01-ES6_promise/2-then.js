198c4437346984ce0fa196d4d39c6dc503fe6ee8
MrtnOmwenga <martin36449@gmail.com>
2023-06-27 21:47:25 +0000

    Then

---
+++ 0x01-ES6_promise/2-then.js
@@ -0,0 +1,8 @@
+const handleResponseFromAPI = (promise) => {
+  promise.then(() => {
+    console.log('Got a response from the API');
+    return ({ status: 200, body: 'success' });
+  }).catch(() => (new Error('')));
+};
+
+export default handleResponseFromAPI;
