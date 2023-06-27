116b425b5a75b1682dc88715d85abfd6c6daee4f
MrtnOmwenga <martin36449@gmail.com>
2023-06-27 22:17:22 +0000

    Ipdated formatting

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,10 +1,5 @@
-const handleResponseFromAPI = (promise) => {
-  return promise.then(() => {
-    ({ status: 200, body: 'success' });
-  }).catch(() => (new Error()))
-	.finally(() => {
-	    console.log("Got a response from the API");
-	});
-};
+const handleResponseFromAPI = (promise) => promise.then(() => ({ status: 200, body: 'success' }))
+  .catch(() => new Error())
+  .finally(() => console.log('Got a response from the API'));
 
 export default handleResponseFromAPI;
