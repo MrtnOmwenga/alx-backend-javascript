37cdb3d0cce98cb637bdf2b59b8924caa558d3fc
MrtnOmwenga <martin36449@gmail.com>
2023-06-29 19:45:29 +0000

    Extends abstract class

---
+++ 0x02-ES6_classes/6-sky_high.js
@@ -0,0 +1,22 @@
+import Building from './5-building';
+
+export default class SkyHighBuilding extends Building {
+  constructor(sqft, floors) {
+    super(sqft);
+    this._floors = floors;
+  }
+
+  // sqft getter
+  get sqft() {
+    return this._sqft;
+  }
+
+  // floors getter
+  get floors() {
+    return this._floors;
+  }
+
+  evacuationWarningMessage() {
+    return 'Evacuate slowly the NUMBER_OF_FLOORS floors';
+  }
+}
