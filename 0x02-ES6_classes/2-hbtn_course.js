6ac65678f3134636f3d6b04869956aa1ab0479a6
MrtnOmwenga <martin36449@gmail.com>
2023-06-29 16:16:57 +0000

    eslint

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -1,63 +1,63 @@
 export default class HolbertonCourse {
-    constructor(name, length, students) {
-	if (typeof(name) === 'string') {
-	    this._name = name;
-	} else {
-	    throw new TypeError("Name must be a string");
-	}
-
-	if (typeof(length) === 'number') {
-	    this._length = length;
-	}else {
-	    throw new TypeError("Length must be a number");
-	}
-
-	if (typeof(students) === 'object') {
-	    this._students = students;
-	}else {
-	    throw new TypeError("Students must be a list");
-	}
+  constructor(name, length, students) {
+    if (typeof (name) === 'string') {
+      this._name = name;
+    } else {
+      throw new TypeError('Name must be a string');
     }
 
-    // name getter
-    get name() {
-	return this._name;
+    if (typeof (length) === 'number') {
+      this._length = length;
+    } else {
+      throw new TypeError('Length must be a number');
     }
 
-    // name setter
-    set name(name) {
-	if (typeof(name) === 'string') {
-	    this._name = name;
-	}else {
-	    throw new TypeError("Name must be a string")
-	}
+    if (typeof (students) === 'object') {
+      this._students = students;
+    } else {
+      throw new TypeError('Students must be a list');
     }
+  }
 
-    // length getter
-    get length() {
-	return this._length;
-    }
+  // name getter
+  get name() {
+    return this._name;
+  }
 
-    // length setter
-    set length(length) {
-	if (typeof(length) === 'number') {
-	    this._length = length;
-	}else {
-	    throw new TypeError("Length must be a number");
-	}
+  // name setter
+  set name(name) {
+    if (typeof (name) === 'string') {
+      this._name = name;
+    } else {
+      throw new TypeError('Name must be a string');
     }
+  }
 
-    // students getter
-    get students() {
-	return this._students;
-    }
+  // length getter
+  get length() {
+    return this._length;
+  }
 
-    // students setter
-    set students(students) {
-	if (typeof(students) === 'object') {
-	    this._students = students;
-	}else {
-	    throw new TypeError("Students must be a list");
-	}
+  // length setter
+  set length(length) {
+    if (typeof (length) === 'number') {
+      this._length = length;
+    } else {
+      throw new TypeError('Length must be a number');
     }
+  }
+
+  // students getter
+  get students() {
+    return this._students;
+  }
+
+  // students setter
+  set students(students) {
+    if (typeof (students) === 'object') {
+      this._students = students;
+    } else {
+      throw new TypeError('Students must be a list');
+    }
+  }
 }
