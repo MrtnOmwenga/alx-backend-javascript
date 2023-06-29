ad9d10977773e68fa591179453fd6cb023687001
MrtnOmwenga <martin36449@gmail.com>
2023-06-29 18:28:48 +0000

    Pricing

---
+++ 0x02-ES6_classes/4-pricing.js
@@ -0,0 +1,30 @@
+export default class Pricing {
+  constructor(amount, currency) {
+    this._amount = amount;
+    this._currency = currency;
+  }
+
+  // amount getter
+  get amount() {
+    return this._amount;
+  }
+
+  // amount setter
+  set amount(newAmount) {
+    this._amount = newAmount;
+  }
+
+  // currency getter
+  get currency() {
+    return this._currency;
+  }
+
+  // currency setter
+  set currency(newCurrency) {
+    return this._currency;
+  }
+
+  displayFullPrice() {
+    return `${this._amount} ${this._currency.name} (${this._currency.code})`;
+  }
+}
