19b0fd558f7a2c888d6d6d44a95f97426d5a16be
MrtnOmwenga <martin36449@gmail.com>
2023-06-29 19:20:53 +0000

    Eslint

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,18 +1,18 @@
 export default class Building {
-    constructor(sqft) {
-	if (this.constructor !== Building && !this.evacuationWarningMessage) {
-	    throw new Error("Class extending Building must override evacuationWarningMessage");
-	}
-	this._sqft = sqft;
+  constructor(sqft) {
+    if (this.constructor !== Building && !this.evacuationWarningMessage) {
+      throw new Error('Class extending Building must override evacuationWarningMessage');
     }
+    this._sqft = sqft;
+  }
 
-    // sqft getter
-    get sqft() {
-	return this._sqft;
-    }
+  // sqft getter
+  get sqft() {
+    return this._sqft;
+  }
 
-    // sqfr setter
-    set sqfg(newSQFT) {
-	this._sqft = newSQFT;
-    }
+  // sqfr setter
+  set sqfg(newSQFT) {
+    this._sqft = newSQFT;
+  }
 }
