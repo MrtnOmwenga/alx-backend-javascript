2d5eed92e40a4748576d13efd95986171ee5f34f
MrtnOmwenga <martin36449@gmail.com>
2023-07-07 19:39:39 +0000

    Handled missing grade property

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -2,7 +2,7 @@ const updateStudentGradeByCity = (students, city, newGrades) => {
     const localStudents = students.filter(student => student.location === city);
     return localStudents.map((student) => {
 	return { ...student,
-		 grade: newGrades.filter((grades) => grades.studentId === student.id)[0]?.grade
+		 grade: newGrades.filter((grades) => grades.studentId === student.id)[0]?.grade || 'N/A'
 	       };
     })
 }
