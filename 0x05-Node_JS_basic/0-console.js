f0050e18789e6c4c2d44b2262a8d735d877ca61d
MrtnOmwenga <martin36449@gmail.com>
2023-08-22 20:19:30 +0000

    NodeJS Basics

--- 0x05-Node_JS_basic/0-console.js
+++ 0x05-Node_JS_basic/0-console.js
@@ -4,7 +4,7 @@
  * @author Bezaleel Olakunori <https://github.com/B3zaleel>
  */
 const displayMessage = (msg) => {
-  process.stdout.write(msg);
+  process.stdout.write(msg+"\n");
 };
 
 module.exports = displayMessage;
